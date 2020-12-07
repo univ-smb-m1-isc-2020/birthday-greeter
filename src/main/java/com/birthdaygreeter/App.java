498655a04d17bb23955c9e48ccd25d15cf893133
tristan <tristan.porteries@gmail.com>
2020-12-07 15:04:12 +0100

    Lecture du fichier de personnes

--- src/main/java/com/birthdaygreeter/App.java
+++ src/main/java/com/birthdaygreeter/App.java
@@ -1,13 +1,42 @@
 package com.birthdaygreeter;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Data;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
+    final static String PEOPLE_FILENAME = "birthdays.txt";
+
+    public static List<People> getPeople() {
+
+        ArrayList<People> people = new ArrayList<People>();
+
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(PEOPLE_FILENAME));
+
+            for (String line : lines) {
+                String[] tokens = line.split("; ", 3);
+            
+                people.add(new People(tokens[0], tokens[1], tokens[2]));
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return people;
+    }
+    
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        List<People> people = getPeople();
     }
 }
---
+++ src/main/java/com/birthdaygreeter/People.java
@@ -0,0 +1,18 @@
+package com.birthdaygreeter;
+
+public class People
+{
+
+    private String name;
+    private String email;
+    private String birthday;
+
+    public People(String name, String email, String birthday) {
+        this.name = name;
+        this.email = email;
+        this.birthday = birthday;
+    }
+    
+    public void greet() {
+    }
+};
